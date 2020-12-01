ad747b1b917aee12310efe9cc777500152b523cb
Dewitt Chanell <dechanel@oa-i5.msk.21-school.ru>
2020-12-01 18:46:30 +0300

    1

--- get_next_line.c
+++ get_next_line.c
@@ -11,8 +11,25 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <unistd.h>
 
-int		get_next_line(int fd, char **line)
+int process_cache()
+
+int	get_next_line(int fd, char **line)
 {
-	
+	static char buf[BUFFER_SIZE];
+	static char *cache = 0;
+	int 		res;
+
+	if (cache)
+	{
+		res = process_cache(cache, line);
+		if (res == 1 || res == -1)
+			return (res);
+	}
+	while (read(fd, buf, BUFFER_SIZE))
+	{
+		if (endl_in_str(buf))
+			*line = str_dup(buf, 0, endl_in_str(buf));
+	}
 }
